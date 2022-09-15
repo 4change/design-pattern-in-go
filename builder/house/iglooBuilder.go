483e0c0cde7dfaef9cb4ce1c54bcdb00f9a4c670
4change <e_mail4gxf@163.com>
2022-09-15 14:00:25 +0800

    builder: house

---
+++ builder/house/iglooBuilder.go
@@ -0,0 +1,33 @@
+package main
+
+type IglooBuilder struct {
+    windowType string
+    doorType   string
+    floor      int
+}
+
+func newIglooBuilder() *IglooBuilder {
+    return &IglooBuilder{}
+}
+
+////////////////////////////// 实现 iBuilder 接口中的方法
+
+func (b *IglooBuilder) setWindowType() {
+    b.windowType = "Snow Window"
+}
+
+func (b *IglooBuilder) setDoorType() {
+    b.doorType = "Snow Door"
+}
+
+func (b *IglooBuilder) setNumFloor() {
+    b.floor = 1
+}
+
+func (b *IglooBuilder) getHouse() House {
+    return House{
+        doorType:   b.doorType,
+        windowType: b.windowType,
+        floor:      b.floor,
+    }
+}
